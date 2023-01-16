4f490b3a806e1dbe402ebef487763baf0ee9d6e5
Dylan Landry <dylan.landry@milkmoovement.com>
2023-01-16 08:41:53 -0500

    convert eslint config from json to js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
-{
-  "env": {
+module.exports = {
+  env: {
     "browser": true,
     "es6": true,
     "node": true
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "typescript": {
-        "project": "tsconfig.json"
+      typescript: {
+        project: "tsconfig.json"
       }
     }
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json",
-    "tsconfigRootDir": "./"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: "./"
   },
-  "plugins": [
+  plugins: [
     "@typescript-eslint",
     "import"
   ],
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",

f8ea0d7064e287f3a3317b73458d60e7fb2a6371
dylan <dylanglandry@gmail.com>
2023-01-16 16:59:46 -0500

    show current session time on stopwatch

--- src/features/stopwatch/stopwatch.tsx
+++ src/features/stopwatch/stopwatch.tsx
@@ -1,26 +1,40 @@
 import { useAppDispatch, useAppSelector } from "app/hooks";
-import { removeStopwatch, selectStopwatchIsRunning, startStopwatch, stopStopwatch } from "features/stopwatch/stopwatchesSlice";
+import { removeStopwatch, selectStopwatchCurrentSessionDuration, selectStopwatchIsRunning, startStopwatch, stopStopwatch } from "features/stopwatch/stopwatchesSlice";
+import { useState } from "react";
 
 export const Stopwatch = ({ id }: { id: string }) => {
-	const dispatch = useAppDispatch();
-
 	const isRunning = useAppSelector((state) => selectStopwatchIsRunning(state, id));
+	const currentSessionDuration = useAppSelector((state) => selectStopwatchCurrentSessionDuration(state, id));
 
+	// This is how I get the component to rerender every second despite stopwatch state in redux not changing.
+	// Timeout calls function that modifies a bit of component state, which is the timeout id itself.
+	const [rerenderTimeoutId, setRerenderTimeoutId] = useState<NodeJS.Timeout | null>(null);
+	const startRerenderTimeout = (): void => setRerenderTimeoutId(setTimeout(startRerenderTimeout, 1000));
+
+	const dispatch = useAppDispatch();
 	const onClickRemove = () => dispatch(removeStopwatch(id));
-	const onClickToggleStart = () => isRunning ? dispatch(stopStopwatch(id)) : dispatch(startStopwatch(id));
+	const onClickToggleStart = () => {
+		if (rerenderTimeoutId) clearTimeout(rerenderTimeoutId);
+		if (isRunning) {
+			dispatch(stopStopwatch(id));
+		} else {
+			dispatch(startStopwatch(id));
+			startRerenderTimeout();
+		}
+	}
 
 	return (
 		<div>
-			<span>This is stopwatch: {id}</span>
+			<div>Current Session Duration: {currentSessionDuration}</div>
 			<button onClick={onClickRemove}>x</button>
-			<ToggleStartButton isRunning={isRunning} onClick={onClickToggleStart}/>
+			<ToggleStartButton isRunning={isRunning} onClick={onClickToggleStart} />
 		</div>
 	);
 }
 
 const ToggleStartButton = (
 	{ isRunning, onClick }:
-	{ isRunning: boolean, onClick: () => void }
+		{ isRunning: boolean, onClick: () => void }
 ) => {
 	const label = isRunning ? 'Stop' : 'Start';
 	return (
--- src/features/stopwatch/stopwatchesSlice.ts
+++ src/features/stopwatch/stopwatchesSlice.ts
@@ -1,6 +1,6 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {RootState} from "app/store";
-import {createWidget, Widget, WidgetKind} from "features/widget/widget";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "app/store";
+import { createWidget, Widget, WidgetKind } from "features/widget/widget";
 import remove from 'lodash/remove';
 import moment from 'moment';
 
@@ -52,6 +52,15 @@ export const selectStopwatchIsRunning = (state: RootState, id: string): boolean 
 	return session.end == null;
 }
 
+export const selectStopwatchCurrentSessionDuration = (state: RootState, id: string): DurationMs => {
+	const stopwatch = state.stopwatches.widgets.find(w => w.id === id);
+	if (!stopwatch) return 0 as DurationMs;
+	const session = stopwatch.sessions[stopwatch.sessions.length - 1];
+	if (!session) return 0 as DurationMs;
+	const durationMs = moment(session.end).diff(session.start, 'milliseconds');
+	return durationMs as DurationMs;
+}
+
 type Stopwatch = Widget & {
 	kind: WidgetKind.Stopwatch;
 	sessions: Session[]
@@ -63,8 +72,8 @@ type Session = {
 }
 
 /**
- * Type for storing dates in redux in a serializable form so store peristance
- * and hydration isn't broken. Throws error if you try to store a date.
+ * Used string as the type for storing dates in redux in a serializable form so store peristance
+ * and hydration isn't broken. Throws error if you try to store a Date type.
  * https://redux.js.org/faq/organizing-state#can-i-put-functions-promises-or-other-non-serializable-items-in-my-store-state
  */
 type DateTime = { kind: 'DateTime' } & string;
@@ -73,6 +82,8 @@ const isDateTime = (dateTime: string | DateTime): dateTime is DateTime => {
 	// example: "2023-01-01T20:16:49.114Z"
 	return moment(dateTime, 'YYYY-MM-DDTHH-mm-ss.SSSZ').isValid();
 }
+
+type DurationMs = { kind: 'DurationMs' } & number;
 
 const throwIfNotDateString = (dateString: string | DateTime): DateTime => {
 	if (!isDateTime(dateString)) throw new Error(`string ${dateString} is not a valid DateTime`);
